a24bee0c6cbae2198214fd7fcc3061d0ad063c07
xavier910cookies <92571703+xavier910cookies@users.noreply.github.com>
2026-07-03 12:48:43 +1200

    sprint 2

--- 3 player movement.py
+++ 3 player movement.py
@@ -1,6 +1,6 @@
 # Example file showing a circle moving on screen
 import pygame
-
+import random
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
@@ -18,7 +18,20 @@ player1_speed = 300
 player2_speed = 300
 player3_speed = 1200
 
+#set player colours
+player1_col = "#FF0000FF"
+player2_col = "#0800FFFF"
+player3_col = "#F6FF00FF"
+
+# set tag variables   
+players = ["player1", "player2","player3"]
+it = random.choice(players)
+
+def check_collisions(player):
+    if pygame.rect.collide
+
 while running:
+    
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
@@ -26,11 +39,11 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("#A9A9A9")
+    screen.fill("#FFFFFF")
 
     # Player 1 movement
 
-    pygame.draw.circle(screen, "red", player1_pos, 40)
+    pygame.draw.circle(screen, player1_col, player1_pos, 40)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
@@ -49,7 +62,7 @@ while running:
 
     # Player 2 movement
 
-    pygame.draw.circle(screen, "blue", player2_pos, 40)
+    pygame.draw.circle(screen, player2_col, player2_pos, 40)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP]:
@@ -62,12 +75,13 @@ while running:
         player2_pos.x += player2_speed * dt
 
     # keep player 2 circle within screen
+
     player2_pos.x = max(40, min(player2_pos.x, screen.get_width() - 40))
     player2_pos.y = max(40, min(player2_pos.y, screen.get_height() - 40))
 
     # Player 3 movement
     
-    pygame.draw.circle(screen, "yellow", player3_pos, 40)
+    pygame.draw.circle(screen, player3_col, player3_pos, 40)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_i]:
@@ -79,9 +93,32 @@ while running:
     if keys[pygame.K_l]:
         player3_pos.x += player3_speed * dt
 
-    # keep player 3 circle within screen
+    #keep player 3 circle within screen
     player3_pos.x = max(40, min(player3_pos.x, screen.get_width() - 40))
     player3_pos.y = max(40, min(player3_pos.y, screen.get_height() - 40))
+    
+    #check player collisions
+
+
+    #run tag game
+
+    if it == "player1":
+
+        set('it' "player1")
+        for i in range (1):
+            print(f'{it} is it')
+
+    elif it == "player2":
+
+        set('it' "player2")
+        for i in range (1):
+            print(f'{it} is it')
+
+    elif it == "player3":
+
+        set('it' "player3")
+        for i in range (1):
+            print(f'{it} is it')  
 
     # flip() the display to put your work on screen
     pygame.display.flip()
---
+++ import random.py
@@ -0,0 +1,7 @@
+import random
+
+for i in range(1,100):
+    players = ["player1", "player1", "player1", "player1","player1", "player2","player3"]
+    it = random.choice(players)
+    print(it)
+
